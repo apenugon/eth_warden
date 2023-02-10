cf64df3c7b151c0f9fe2f5bbd9d5e890cd9aa80f
Akul Penugonda <akulvp@gmail.com>
2023-02-10 11:55:12 -0500

    Add circom built files to repo

--- setup_dirs_and_rust.sh
+++ setup_dirs_and_rust.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-mkdir -p circuits/cc_prove_decryption
-pushd circuits/cc_prove_decryption
-circom ../prove_decryption.circom --wasm
-popd
 pushd wasm
 wasm-pack build --out-dir pkg-web
 popd
